e21c8c58c7bf413ad8217fb167fe5c11a4b08aea
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2019-09-18 10:41:54 -0700

    Update Command.java

--- Command.java
+++ Command.java
@@ -1,22 +1,24 @@
-public class Command {
+public class Main {
 
 	public static void main(String[] args) {
-	Fractions myFraction = new Fractions(6, 7);
-	myFraction.GCD(myFraction);
-	Fractions myFraction2 = new Fractions(8, 9);
-	myFraction2.GCD(myFraction2);
+		System.out.println("Prog_Ex 5???");
+	Fractions myFraction = new Fractions(14, 70);
+	Fractions myFraction2 = new Fractions(12, 3);
+
 	
 	//running methods for fraction 1
-	System.out.println(myFraction.getNumerator());
-	System.out.println(myFraction.getDenominator());
+	System.out.println(myFraction.toString());
+	System.out.println(myFraction2.toString());
+	System.out.println(myFraction.getP());
+	System.out.println(myFraction.getQ());
 	System.out.println(myFraction.getDouble());
 	//running methods for fraction 2
-	System.out.println(myFraction2.getNumerator());
-	System.out.println(myFraction2.getDenominator());
+	System.out.println(myFraction2.getP());
+	System.out.println(myFraction2.getQ());
 	System.out.println(myFraction2.getDouble());
-	Fractions.Multiply(myFraction, myFraction2);
+	System.out.println(Fractions.Multiply(myFraction, myFraction2));
+	System.out.println(Fractions.add(myFraction, myFraction2));
 	
 	}//main
 
 }//Command
-
