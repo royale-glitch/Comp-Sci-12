a817e10f33682a624b69fdfa2656f526a18b0067
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2019-09-27 09:37:41 -0700

    Update PE8Obj.java

--- PE8Obj.java
+++ PE8Obj.java
@@ -2,14 +2,10 @@
 public class PE8Obj {
 	private String word;
 	private int score;
-	private int key;
 	
 		
 	public PE8Obj(String myWord, int myScore, int myKey){
 		this.word = myWord;
-		this.score = myScore;
-		this.key = myKey;		
-	}
-	
-	
+		this.score = myScore;		
+	}	
 }
