b4a2b20efc276170256ba8ba5828bf9aff415b48
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2019-10-03 11:27:17 -0700

    Update AddressBook.java

--- ProgrammingEx_10/src/addressbookpackage/AddressBook.java
+++ ProgrammingEx_10/src/addressbookpackage/AddressBook.java
@@ -1,94 +1,30 @@
-ackage addressbookpackage;
-
+package addressbookpackage;
+import java.util.*;
 public class AddressBook {
-
-
-	private String firstName;
-	private String lastName;
-	private String address;
-	private String address2;
-	private String city;
-	private String province;
-	private String country;
-	private String postalCode;
+	  
+	private ArrayList <OneAddress> addressBook = new ArrayList<OneAddress>();
+	//checks if someone is in the book;
 	
-	//default constructor
-	AddressBook(){
-		this.firstName = "";
-		this.lastName = "";
-		this.address = "";
-		this.address2 = "";
-		this.city = "";
-		this.province = "";
-		this.country = "";
-		this.postalCode= "";
-	}
-	
-	public AddressBook(String myFirstName, String myLastName, String myAddress, String myAddress2, String myCity, String myProvince, String myCountry, String myPostalCode){
-		this.firstName = myFirstName;
-		this.lastName = myLastName;
-		this.address = myAddress;
-		this.address2 = myAddress2;
-		this.city = myCity;
-		this.province = myProvince;
-		this.country = myCountry;
-		this.postalCode= myPostalCode;
+	public boolean inBook(String firstName, String lastName) {
+		boolean isBook = false;
 		
-	}
+		for(OneAddress i : addressBook) {
+				if(i.getFirstName().equals(firstName) && i.getLastName().equals(lastName)) {
+					isBook = true;
+				} else {isBook = false;}
+			
+			}
+		return isBook;
+		
+	}//inBook	
 	
-	//all getters
-	public String getFirstName() {
-		return this.firstName;
-	}
+	public void addAddress(String lastName, String firstName, String address, String address2, 
+						   String city, String province, String country, String postalCode) {
+		OneAddress newAddress = new OneAddress(lastName, firstName, address, address2, 
+											   city, province, country, postalCode);
+		addressBook.add(newAddress);
+		
+	}//addAddress
 	
-	public String getLastName() {
-		return this.lastName;
-	}
-	public String getStreetAddress() {
-		return this.address;
-	}
-	public String getStreetAddress2() {
-		return this.address2;
-	}
-	public String getCity() {
-		return this.city;
-	}
-	public String getProvince() {
-		return this.province;
-	}
-	public String getCountry() {
-		return this.country;
-	}
-	public String getPostalCode() {
-		return this.postalCode;
-	}
-	
-	//all setters
-	public void setFirstName(String myFirstName) {
-		this.firstName = myFirstName;
-	}
-	
-	public void setLastName(String myLastName) {
-		this.lastName = myLastName;
-	}
-	public void setAddress(String myAddress) {
-		this.address = myAddress;
-	}
-	public void setAddress2(String myAddress2) {
-		this.address2 = myAddress2;
-	}
-	public void setCity(String myCity) {
-		this.city = myCity;
-	}
-	public void setProvince(String myProvince) {
-		this.province = myProvince;
-	}
-	public void setCountry(String myCountry) {
-		this.country = myCountry;
-	}
-	public void setPostalCode(String myPostalCode) {
-		this.postalCode = myPostalCode;
-	}
-	
-}//oneAddress
-}
+
+}//AddressBook
