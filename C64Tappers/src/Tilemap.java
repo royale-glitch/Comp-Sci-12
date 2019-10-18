edf77414c4fa9b5bfbe0cd20a1b6bcc40725743b
royale-glitch <52724894+royale-glitch@users.noreply.github.com>
2019-10-18 09:39:15 -0700

    Add files via upload

---
+++ C64Tappers/src/Tilemap.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class Tilemap {
+	private int x;
+	private int y;
+	private boolean value;
+	
+	//getters
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public boolean getValue(){
+		return this.value;
+	}
+	
+	//setters
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setValue(boolean value) {
+		this.value = value;
+	}
+	
+	
+	
+	
+}//Tilemap
